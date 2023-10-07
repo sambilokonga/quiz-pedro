11951c71b2412a6d777e4240e606a95e8b983f29
sambilokonga <abebawbegna@gmail.com>
2023-10-07 23:30:34 +0300

    all components

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import { QuizContext } from "./Tools/Contexts";
 
 function App() {
   const [gameState, setGameState] = useState("menu");
+  const [score, setScore] = useState(0);
 
   return (
     <div className="App">
       <h1>Quiz App</h1>
-      <QuizContext.Provider value={{ gameState, setGameState }}>
+      <QuizContext.Provider
+        value={{ gameState, setGameState, score, setScore }}
+      >
         {gameState === "menu" && <HomeMenu />}
         {gameState === "exam" && <Exam />}
         {gameState === "finishExam" && <FinishExam />}
---
+++ src/Tools/QuestionsList.js
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: "Which of the following is lower level programming?",
+    optionA: "Python",
+    optionB: "C-programming",
+    optionC: "JavaScript",
+    optionD: "Java",
+    asnwer: "optionB",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    asnwer: "optionD",
+  },
+];
