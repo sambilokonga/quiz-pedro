c7b0fce152da272860b110497d6cf0626594aa93
sambilokonga <abebawbegna@gmail.com>
2023-10-07 14:25:18 +0300

    creating home menu component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import React, { useState, useContext } from "react";
+import HomeMenu from "./Components/HomeMenu";
+import Exam from "./Components/Exam";
+import FinishExam from "./Components/FinishExam";
+import { QuizContext } from "./Tools/Contexts";
 
 function App() {
+  const [gameState, setGameState] = useState("menu");
+
   return (
     <div className="App">
       <h1>Quiz App</h1>
+      <QuizContext.Provider value={{ gameState, setGameState }}>
+        {gameState === "menu" && <HomeMenu />}
+        {gameState === "exam" && <Exam />}
+        {gameState === "finishExam" && <FinishExam />}
+      </QuizContext.Provider>
     </div>
   );
 }
---
+++ src/Components/FinishExam.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FinishExam() {
+  return <div>FinishExam</div>;
+}
+
+export default FinishExam;
